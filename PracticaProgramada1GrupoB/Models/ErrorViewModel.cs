221bfeaf0db9cede28047e8212434a6b8610576b
Horacio <157430552+hporrasm@users.noreply.github.com>
2026-02-12 21:43:47 -0600

    Add project files.

---
+++ PracticaProgramada1GrupoB/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PracticaProgramada1GrupoB.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
